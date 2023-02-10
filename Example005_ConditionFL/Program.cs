7fdc3363f71943ce2882d5c08a125dc4c8ee35be
Gosha1990 <123661130+Gosha1990@users.noreply.github.com>
2023-02-10 17:48:31 +0300

    Base

---
+++ Example005_ConditionFL/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("Введите имя пользователя:");
+ string username = Consol.ReadLine();
+
+if(username == "Маша") ;
+ { 
+    Console.WriteLine("ура! Это же маша") ;
+    
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);  
+}
+    
